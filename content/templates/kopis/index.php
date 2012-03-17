aa4613a5f561e51a160e767f5acb6432db23df7c
Carsten Ringe <carsten.ringe@web.de>
2012-03-17 10:19:15 +0100

    Switch to JQuery Mobile

--- content/templates/kopis/index.php
+++ content/templates/kopis/index.php
@@ -2,16 +2,20 @@
 <html> 
 <head> 
 <title><?php template_sitename(); ?></title> 
-<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=1;"/> 
+<meta name="viewport" content="width=device-width, initial-scale=1;"/> 
 <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-<link rel="apple-touch-icon" href="<?php echo Templates::get_url('iui/iui-logo-touch-icon.png'); ?>" /> 
-<link rel="stylesheet" href="<?php echo Templates::get_url('iui/iui.css'); ?>" type="text/css" /> 
-<script type="application/x-javascript" src="<?php echo Templates::get_url('iui/iui.js'); ?>"></script> 
+<!--
 <link rel="stylesheet" href="<?php echo Templates::get_url('style.css'); ?>" type="text/css" />
+-->
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0.1/jquery.mobile-1.0.1.min.css" />
+<script src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
+<script src="http://code.jquery.com/mobile/1.0.1/jquery.mobile-1.0.1.min.js"></script>
 <?php template_header(); ?>
 </head> 
 
 <body> 
+<div data-role="page">
+<div data-role="header">
     <div class="toolbar"> 
         <h1 id="pageTitle"></h1> 
         <a id="backButton" class="button" href="<?php template_siteurl(); ?>"></a> 
@@ -24,34 +28,32 @@
 	<li><a href="<?php template_siteurl(); ?>index.php?hours=48"><?php printf(_r('Past %d hours'), 48) ?></a></li>
 	<li><a href="<?php template_siteurl(); ?>index.php?hours=168"><?php _e('Past week') ?></a></li>
 	<li><a href="<?php template_siteurl(); ?>index.php?hours=-1"><?php _e('Show all') ?></a></li>
-	<li><a href="<?php template_siteurl(); ?>index.php?method=update&cron"><?php _e('Update'); ?></a></li>
+	<li><a href="<?php template_siteurl(); ?>index.php?method=update"><?php _e('Update'); ?></a></li>
         <li class="last"><a href="admin/">Admin</a></li> 
-    </ul> 
+    </ul>
+</div><!-- header -->
 
-<div id="contents"></div>
-
-    <ul id="articles" title="Articles"> 
+<div data-role="content">
+    <ul data-role="listview" data-inset="true">
 <?php while(has_items()): the_item(); ?>
-	<?php the_date('before=<li class="group" title="' . _r('Click to expand/collapse date') . '" class="date"> &after=</li>&format=l d F, Y') ?>
-	<li><h1><a href="#article-<?php the_id(); ?>"><?php the_title(); ?></a></h1></li>
-<div id="article-<?php the_id(); ?>" class="panel article" title="<?php the_title(); ?>"><?php the_content(); ?><?php if(has_enclosure()) { ?><?php _the_enclosure(); ?><?php } ?></div>
-<!--
-<script type="javascript">
-content = document.createElement('div');
-content.setAttribute('id', 'article-<?php the_id(); ?>');
-content.setAttribute('class', 'panel');
-content.setAttribute('title', '<?php the_title(); ?>');
-content.innerHTML = '<?php the_content(); ?><?php if(has_enclosure()) { ?><?php _the_enclosure(); ?><?php } ?>';
-document.getElementById('contents').appendChild(content);
-</script>
--->
-
+	<?php the_date('before=<li data-role="list-divider" title="' . _r('Click to expand/collapse date') . '"> &after=</li>&format=l d F, Y') ?>
+	<li>
+	<h1 id="article-<?php the_id(); ?>"><?php the_feed_name() ?> - <?php the_title(); ?></h1>
+	<?php the_content(); ?><?php if(has_enclosure()) { ?><?php the_enclosure(); ?><?php } ?>
+	</li>
 <?php endwhile; ?> 
     </ul>
+</div><!-- content -->
 
-	<ul id="feeds" title="feeds">
+<div data-role="page" id="feeds">
+<ul title="feeds">
 <?php list_feeds('format=<li><a href="%1$s">%3$s</a><a href="%4$2" class="feed-link">('._r('Feed').')</a></li>'); ?>
-	</ul>
+</ul>
+</div>
+
+<div data-role="footer">
+</div>
+</div><!-- page -->
 </body>
 </html>
 
