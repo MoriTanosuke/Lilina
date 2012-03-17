a80814810e8e0c506c901f086d08822cf80cc099
Carsten Ringe <carsten.ringe@web.de>
2012-03-17 17:40:27 +0100

    Fix hotkeys. And do a simpler mobile layout.

--- content/templates/kopis/hotkeys.js
+++ content/templates/kopis/hotkeys.js
@@ -4,21 +4,30 @@ $(document).keypress(function(data) {
    case 63: console.log('help');
      break;
    case 106:
-     if(kopis.current_article_pos <= kopis.max_article_pos) $.scrollTo($('div#articles h1')[kopis.current_article_pos++]);
+     if(kopis.current_article_pos < kopis.max_article_pos) {
+       $('div.article:nth-child(' + kopis.current_article_pos + ')').trigger('collapse');
+       kopis.current_article_pos++;
+       $('div.article:nth-child(' + kopis.current_article_pos + ')').trigger('expand');
+       $.scrollTo($('div.article:nth-child(' + kopis.current_article_pos + ')'));
+     }
      break;
    case 107:
-     if(kopis.current_article_pos > kopis.min_article_pos) kopis.current_article_pos--;
-     $.scrollTo($('div#articles h1')[kopis.current_article_pos]);
+     if(kopis.current_article_pos >= kopis.min_article_pos) {
+       $('div.article:nth-child(' + kopis.current_article_pos + ')').trigger('collapse');
+       kopis.current_article_pos--;
+       $('div.article:nth-child(' + kopis.current_article_pos + ')').trigger('expand');
+       $.scrollTo($('div.article:nth-child(' + kopis.current_article_pos + ')'));
+     }
      break;
    default:
      return true;
  }
 });
 
-kopis.max_article_pos = $('div#articles h1').size()
+kopis.max_article_pos = $('div.article').size()
 }
 
 kopis = {}
-kopis.current_article_pos = 0;
-kopis.min_article_pos = 0;
+kopis.current_article_pos = 1;
+kopis.min_article_pos = 1;
 
--- content/templates/kopis/index.php
+++ content/templates/kopis/index.php
@@ -16,46 +16,31 @@
 <body> 
 <div data-role="page">
 <div data-role="header">
-    <div class="toolbar"> 
-        <h1 id="pageTitle"></h1> 
-        <a id="backButton" class="button" href="<?php template_siteurl(); ?>"></a> 
-    </div> 
-
-    <ul id="home" title="Home" selected="true"> 
-        <li><a href="#articles">Articles</a></li> 
-	<li><a href="#feeds">Feeds</a></li>
-	<li><a href="<?php template_siteurl(); ?>index.php?hours=24"><?php printf(_r('Past %d hours'), 24) ?></a></li>
-	<li><a href="<?php template_siteurl(); ?>index.php?hours=48"><?php printf(_r('Past %d hours'), 48) ?></a></li>
-	<li><a href="<?php template_siteurl(); ?>index.php?hours=168"><?php _e('Past week') ?></a></li>
-	<li><a href="<?php template_siteurl(); ?>index.php?hours=-1"><?php _e('Show all') ?></a></li>
-	<li><a href="<?php template_siteurl(); ?>index.php?method=update"><?php _e('Update'); ?></a></li>
-        <li class="last"><a href="admin/">Admin</a></li> 
-    </ul>
-</div><!-- header -->
-
-<div data-role="content" id="articles">
-    <ul data-role="listview" data-inset="true">
-<?php while(has_items()): the_item(); ?>
-	<?php the_date('before=<li data-role="list-divider" title="' . _r('Click to expand/collapse date') . '"> &after=</li>&format=l d F, Y') ?>
-	<li data-role="list-divider"><h1 id="article-<?php the_id(); ?>"><img src="<?php the_feed_favicon(); ?>" />&nbsp;<?php the_feed_name() ?> - <?php the_title(); ?> at <?php the_date('&format=h:m:s'); ?></h1></li>
-	<li><div class="article"><?php the_content(); ?><?php if(has_enclosure()) { ?><?php the_enclosure(); ?><?php } ?></div>
-	</li>
-<?php endwhile; ?> 
-    </ul>
-</div><!-- content -->
-
-<div data-role="page" id="feeds">
-<ul title="feeds">
-<?php list_feeds('format=<li><a href="%1$s">%3$s</a><a href="%4$2" class="feed-link">('._r('Feed').')</a></li>'); ?>
+<ul>
+<li><a href="<?php template_siteurl(); ?>"><?php template_sitename(); ?></a></li>
+<li><a href="?hours=6">6h</a></li>
+<li><a href="?hours=24">24h</a></li>
+<li><a href="?hours=48">48h</a></li>
+<li><a href="admin/"><?php _e('Admin'); ?></a></li>
 </ul>
+</div><!-- header -->
+<div data-role="content" title="hours-<?php echo get_offset(true); ?>">
+<?php while(has_items()): the_item(); ?>
+<div class="article" data-role="collapsible" data-theme="b" data-content-theme="c">
+  <h1 id="#article-<?php the_id(); ?>"><img src="<?php the_feed_favicon(); ?>" /><?php the_feed_name(); ?> - <?php the_title(); ?> at <?php the_date(); ?></h1>
+  <?php the_content(); ?><?php if(has_enclosure()) { the_enclosure(); } ?>
+  <?php action_bar() ?>
 </div>
-
+<?php endwhile; ?> 
+</div><!-- content -->
 <div data-role="footer">
-</div>
+<ul>
+<li><a href="http://kopis.de">made by kopis.de</a></li>
+</ul>
+</div><!-- footer -->
 </div><!-- page -->
 <script type="text/javascript">
-$(document).ready(function (){init_keys();});
+$(document).ready(function() {init_keys();});
 </script>
 </body>
 </html>
-
